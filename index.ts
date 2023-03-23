75dc4499ef3f6ed7c925a29c128d2721b19d1512
chris-windsor <ccw3102@icloud.com>
2023-03-22 18:11:36 -0600

    Added support for radio style fields and font size

--- index.ts
+++ index.ts
@@ -2,39 +2,63 @@ import { load as loadEnv } from "https://deno.land/std/dotenv/mod.ts";
 import { PDFDocument, StandardFonts, rgb } from "npm:pdf-lib";
 
 const NO_FIELD_VALUE = 'no field value specified';
+const FILL_COLOR = rgb(0.2, 0.1, 0.5); // Blue
+// const FILL_COLOR = rgb(0.95, 0.1, 0.1); // Red
 
-type PositionMap = Map<string, number[]>;
+type TextMap = Map<string, number[]>;
+type RadioMap = Map<string, number[][]>;
 type ValueMap = Map<string, string>;
 
 type SettingsMap = {
     input: string,
     output: string,
-    positions: PositionMap,
+    textMap: TextMap,
+    radioMap: RadioMap,
     values: ValueMap
+}
+
+function filterDataLines(line: string) {
+    const trimmedLine = line.trim();
+    return trimmedLine.length && !trimmedLine.startsWith("#");
 }
 
 async function buildDataMaps() {
     const configData = await loadEnv();
 
-    const positions = await Deno.readTextFile(configData['positions'] || './positions');    
-    const positionsCollected = positions
-        .split("\n")
-        .map(line => {
-            const [key, x, y] = line.split(",");
-            return [key, [parseInt(x), parseInt(y)]];
-        });
-    const positionsMap: PositionMap = new Map((positionsCollected as Iterable<readonly [string, number[]]>));
+    const positions = await Deno.readTextFile(configData['positions'] || './positions');
+    const textFields: TextMap = new Map([]);
+    const radioFields: RadioMap = new Map([]);
+    for (const [lineIdx, rawLine] of positions.split("\n").entries()) {
+        const line = rawLine.trim();
+        if (!line.length || line.startsWith("#")) {
+            continue;
+        } 
+
+        const [type, key, page, x, y, size] = line.split(",");
+
+        if (type === 't') {
+            textFields.set(key, [parseInt(page), parseFloat(x), parseFloat(y), parseInt(size)]);            
+        } else if (type === 'r') {
+            const currentRadioField = radioFields.get(key) || [];
+            radioFields.set(key, currentRadioField.concat([[parseInt(page), parseFloat(x), parseFloat(y), parseInt(size)]]));
+        } else {
+            console.error(`Encounted unknown field type: '${type}' at line ${lineIdx + 1}`);
+        }
+
+    }
     
     const values = await Deno.readTextFile(configData['values'] || './values');
     const valuesCollected = values
         .split("\n")
+        .filter(filterDataLines)
         .map(line => line.split(",")) ;    
     const valuesMap: ValueMap = new Map((valuesCollected as Iterable<readonly [string, string]>));
 
     return {
         input: configData['input'],
         output: configData['output'],
-        positions: positionsMap,
+        textMap: textFields,
+        radioMap: radioFields,
         values: valuesMap,
     }
 }
@@ -45,18 +69,42 @@ async function modifyPdf(settings: SettingsMap) {
     const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
 
     const pages = pdfDoc.getPages();
-    const firstPage = pages[0];
-    const {height} = firstPage.getSize();
     
-    for (const field of settings.positions.entries()) {
-        const [fieldName, [x, y]] = field;
+    for (const field of settings.textMap.entries()) {
+        const [fieldName, [pageIndexAbsolute, x, y, size]] = field;
+
+        const page = pages[pageIndexAbsolute - 1];
+        const { height } = page.getSize();
+
+        const textToDraw = settings.values.get(fieldName) || NO_FIELD_VALUE;
         
-        firstPage.drawText((settings.values.get(fieldName) || NO_FIELD_VALUE), {
+        page.drawText(textToDraw, {
             x,
             y: height - y,
-            size: 12,
+            size: size || 9,
             font: helveticaFont,
-            color: rgb(0.95, 0.1, 0.1),
+            color: FILL_COLOR,
+        });        
+    }
+
+    for (const field of settings.radioMap.entries()) {
+        const [fieldName, radioPositions] = field;
+        const selectedIndex = settings.values.get(fieldName);
+
+        if (!selectedIndex) {
+            continue;
+        }
+
+        const [pageIndexAbsolute, x, y, size] = radioPositions[parseInt(selectedIndex)];
+
+        const page = pages[pageIndexAbsolute - 1];
+        const { height } = page.getSize();
+
+        page.drawSquare({
+            x,
+            y: height - y,
+            size: size || 10,
+            color: FILL_COLOR,
         });
     }
 
@@ -64,10 +112,17 @@ async function modifyPdf(settings: SettingsMap) {
     await Deno.writeFile(settings.output, pdfBytes, { mode: 0o644 });
 }
 
-async function main() {
+async function buildAndModify() {
     const SETTINGS = Object.freeze(await buildDataMaps());
-
     modifyPdf(SETTINGS);
+}
+
+async function main() {
+    buildAndModify();
+    const propertiesWatcher = Deno.watchFs(['./positions', './values']);
+    for await(const _event of propertiesWatcher) {
+        buildAndModify();
+    }
 }
 
 main();
